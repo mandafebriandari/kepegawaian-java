096488652a8195b13438b1317e40cfd7e1fd36f7
manda <manda.purawiraja@gmail.com>
2015-08-12 13:36:07 +0700

    Revisi class

---
+++ kepegawaian.aplikasi/src/main/java/com/askrindo/kepegawaian/Entity/Karyawan.java
@@ -0,0 +1,7 @@
+package com.askrindo.kepegawaian;
+
+public class Karyawan {
+    public static void main(String[] args) {
+        System.out.println("Hello, Manda!");
+    }
+}
---
+++ kepegawaian.aplikasi/src/main/java/com/askrindo/kepegawaian/Entity/UnitKerja.java
@@ -0,0 +1,7 @@
+package com.askrindo.kepegawaian;
+
+public class UnitKerja {
+    public static void main(String[] args) {
+        System.out.println("Hello, Manda!");
+    }
+}
