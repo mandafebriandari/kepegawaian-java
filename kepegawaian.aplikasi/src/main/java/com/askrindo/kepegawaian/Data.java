195797fda5f6851a5b7a21728face5f8434b10ed
abdullahsukamto <abdullahsukamto@gmail.com>
2015-08-12 13:26:35 +0700

    menambah folder entity dan folder service serta memindahkan file domain ke folder entity

--- kepegawaian.aplikasi/src/main/java/com/askrindo/kepegawaian/Data.java
+++
@@ -1,7 +0,0 @@
-package com.askrindo.kepegawaian;
-
-public class Data {
-    public static void main(String[] args) {
-        System.out.println("Hello, Manda!");
-    }
-}
--- kepegawaian.aplikasi/src/main/java/com/askrindo/kepegawaian/UnitKerja.java
+++
@@ -1,7 +0,0 @@
-package com.askrindo.kepegawaian;
-
-public class UnitKerja {
-    public static void main(String[] args) {
-        System.out.println("Hello, Manda!");
-    }
-}
